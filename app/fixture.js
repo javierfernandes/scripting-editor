96a38a3d8edd02534dfa6b7d3e8b9602b6e315a7
Javier Fernandes <javier.fernandes@gmail.com>
2017-08-13 09:58:31 -0300

    Adding dialog nodes

--- app/fixture.js
+++ app/fixture.js
@@ -1,10 +1,12 @@
 export default `<html><body>
-
   <div data-type="scene">
     <div data-type="sceneHeading">EXT. BRICK'S POOL - DAY</div>
     <div data-type="sceneDescription">
       Steel, in the middle of a heated phone call:
     </div>
+
+    <div data-type="dialogTitle">SANDRA</div>
+    <div data-type="dialogText">How did you get here?</div>
   </div>
 
 </body></html>`
