711a107cad5032caba06d9a8771e059bcbe278f4
Javier Fernandes <javier.fernandes@gmail.com>
2017-08-18 09:04:14 -0300

    Reorganizing code into folders

--- app/app.js
+++ app/app.js
@@ -3,19 +3,13 @@ import SimpleWriter from '../lib/simple-writer/SimpleWriter'
 import SimpleWriterPackage from '../lib/simple-writer/SimpleWriterPackage'
 import fixture from './fixture'
 
-let cfg = new Configurator()
+const cfg = new Configurator()
 cfg.import(SimpleWriterPackage)
 
 window.onload = function() {
-  // Import article from HTML markup
-  let importer = cfg.createImporter('html')
-  let doc = importer.importDocument(fixture)
-  // This is the data structure manipulated by the editor
-  let editorSession = new EditorSession(doc, {
-    configurator: cfg
-  })
-  // Mount SimpleWriter to the DOM and run it.
-  SimpleWriter.mount({
-    editorSession: editorSession
-  }, document.body)
+  const importer = cfg.createImporter('html')
+  const doc = importer.importDocument(fixture)
+  
+  const editorSession = new EditorSession(doc, { configurator: cfg })
+  SimpleWriter.mount({ editorSession: editorSession }, document.body)
 }
--- make.js
+++ make.js
@@ -1,30 +1,21 @@
 const b = require('substance-bundler')
 
-b.task('clean', function() {
-  b.rm('./dist')
-})
+b.task('clean', () => b.rm('./dist'))
 
 // copy assets
-b.task('assets', function() {
+b.task('assets', () => {
   b.copy('app/index.html', './dist/index.html')
   b.copy('./node_modules/font-awesome', './dist/font-awesome')
 })
 
-b.task('build', ['clean', 'assets'], function() {
-  _client(false)
-})
-
-b.task('dev:build', ['clean', 'assets'], function() {
-  _client(true)
-})
+b.task('build', ['clean', 'assets'], () => _client(false))
+b.task('dev:build', ['clean', 'assets'], () => _client(true))
 
 b.task('default', ['build'])
 
 // starts a server when CLI argument '-s' is set
 b.setServerPort(5555)
-b.serve({
-  static: true, route: '/', folder: 'dist'
-})
+b.serve({ static: true, route: '/', folder: 'dist' })
 
 function _client(devMode) {
   b.css('./app/app.css', 'dist/app.css', { variables: true })
@@ -34,6 +25,8 @@ function _client(devMode) {
       format: 'umd',
       moduleName: 'app'
     },
-    buble: !devMode && { objectAssign: 'Object.assign' }
+    buble: !devMode && { 
+      objectAssign: 'Object.assign'
+    }
   })
 }
